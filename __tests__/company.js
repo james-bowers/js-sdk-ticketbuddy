669452acb1b16101a92f5fb7acea3eabb63c1540
James Bowers <james@james-bowers.me>
2019-04-09 06:34:32 +0100

    update package name

--- __tests__/company.js
+++ __tests__/company.js
@@ -1,16 +1,12 @@
 const { company } = require("../src");
 
 describe("Company", () => {
-  describe("success", () => {});
-
-  describe("failure", () => {
-    test("creates a company", async () => {
-      const [success, body] = await company.create();
-      expect(success).toEqual(false);
-      expect(body).toEqual({
-        description: "Invalid request",
-        content: { id: ["can't be blank"] }
-      });
-    });
+  test("creates a company", async () => {
+    const [isSuccessful, body] = await company.create();
+    expect(isSuccessful).toEqual(true);
+    // expect(body).toEqual({
+    //   description: "Invalid request",
+    //   content: { id: ["can't be blank"] }
+    // });
   });
 });
